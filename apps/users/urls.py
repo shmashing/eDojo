7f4556640e7bc4150aa2d358a05dc1c34fd40022
Kyle <kyle.mcfar1291@gmail.com>
2017-07-28 11:52:09 -0700

    added forms to make stores

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   url(r'^$', views.index, name='home'),
   url(r'edit_account$', views.edit_user, name='editUser'),
   url(r'^log_in$', views.log_in, name='login'),
+  url(r'^add_store$', views.add_store, name='makeStore'),
   url(r'^logout$', views.logout, name='logout'),
   url(r'^register$', views.register, name='register'),
   url(r'^register/add_user$', views.add_user, name='addUser'),
